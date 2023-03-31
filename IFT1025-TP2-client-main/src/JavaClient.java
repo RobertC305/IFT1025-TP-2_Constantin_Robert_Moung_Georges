aeb84baca9fa879d91a2f638ed2b695434738a5c
Moung-Georges-Rychel <moung.georges@hotmail.fr>
2023-03-31 14:40:44 -0400

    Modification de classe JavaClient
    Copie du code JavaClient fournie par le prof

--- IFT1025-TP2-client-main/src/JavaClient.java
+++ IFT1025-TP2-client-main/src/JavaClient.java
@@ -1,5 +1,44 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class JavaClient {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            Socket cS = new Socket("127.0.0.1",1337);
+
+            OutputStreamWriter os = new OutputStreamWriter(cS.getOutputStream());
+
+            BufferedWriter bw = new BufferedWriter(os);
+
+            Scanner sc = new Scanner(System.in);
+
+            while(sc.hasNext()) {
+                String line = sc.nextLine();
+                System.out.println("J'ai envoyé "+line );
+                bw.append(line+"\n");
+                bw.flush();
+                if(line.equals("exit")) {
+                    System.out.println("Au revoir.");
+                    break;
+                }
+            }
+
+            bw.close();
+            sc.close();
+            cS.close();
+
+        } catch (ConnectException x) {
+            System.out.println("Connexion impossible sur port 1337: pas de serveur.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
