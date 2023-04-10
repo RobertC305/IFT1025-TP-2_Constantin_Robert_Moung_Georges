dd2e8ceecc3c44bee8f8e707f7ee97c196afcc29
Moung-Georges-Rychel <moung.georges@hotmail.fr>
2023-04-10 15:37:59 -0400

    Ajout de la classe ClientApplication

---
+++ IFT1025-TP2-clientFX/src/main/java/ClientApplication.java
@@ -0,0 +1,32 @@
+import Controller.Controller;
+import Model.Client;
+import Model.Course;
+import View.View;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClientApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        View view = new View();
+        //Client client = new Client();
+        //Course course = new Course();
+        //Controller controller = new Controller();
+
+        Scene scene = new Scene(view, 500, 600);
+
+        stage.setScene(scene);
+        stage.setTitle("Application Client");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
